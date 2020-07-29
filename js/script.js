a6d75de089ef18455730c03d28b120afa1da163c
Frederic Iniesta <fredini06@gmail.com>
2020-07-29 15:21:24 +0200

    navbar et footer

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
+
+const section = document.querySelector("section");
 let xhr = new XMLHttpRequest();
-let section = document.querySelector("section");
 
-xhr.open("GEt", "http://localhost:3000/api/teddies");
+xhr.open("GET", "http://localhost:3000/api/teddies");
+xhr.responseType = 'json';
+xhr.send();
+xhr.onload = function() {
+    let teddies = xhr.response;
+    showTeddies(teddies);
+};
 
+
+function showTeddies(jsonObj) {
+    let myH1 = document.createElement("h1");
+    myH1.textContent = jsonObj[0]['colors'][2];
+    section.appendChild(myH1);
+};
