fe15b0420d9d5b070d54a7d257bb1a18d222ea11
Frederic Iniesta <fredini06@gmail.com>
2020-07-31 15:52:32 +0200

    test Clonage de la div

--- js/script.js
+++ js/script.js
@@ -1,24 +1,59 @@
 
-const section = document.querySelector("section");
-let xhr = new XMLHttpRequest();
-
-xhr.open("GET", "http://localhost:3000/api/teddies");
-xhr.responseType = 'json';
-xhr.send();
-xhr.onload = function() {
-    let teddies = xhr.response;
-    showTeddies(teddies);
-};
+// xhr.open("GET", "http://localhost:3000/api/teddies");
+// xhr.responseType = 'json';
+// xhr.send();
+// xhr.onload = function() {
+//     let teddies = xhr.response;
+//     showTeddies(teddies);
+// };
 
 
-function showTeddies(jsonObj) {
-    document.getElementById("name").innerHTML = jsonObj[0]['name'];
-    document.getElementById("description").innerHTML = jsonObj[0]['description'];
-    document.getElementById("price").innerHTML = jsonObj[0]['price'];
+// function showTeddies(jsonObj) {
+//     document.getElementById("name").innerHTML = jsonObj[0]['name'];
+//     document.getElementById("description").innerHTML = jsonObj[0]['description'];
+//     document.getElementById("price").innerHTML = jsonObj[0]['price'];
     
-    let im = document.getElementById("image");
-    let imUrl = jsonObj[0]['imageUrl'];
-    im.src = imUrl;
-};
+//     let im = document.getElementById("image");
+//     let imUrl = jsonObj[0]['imageUrl'];
+//     im.src = imUrl;
+// };
+
+fetch("http://localhost:3000/api/teddies")
+    .then(function (response) {
+        response.json()
+            .then(function (value) {
+                let tab = value;
+                showTeddies(tab);
+            })            
+    });
+
+// function showTeddies(tab){
+//     document.getElementById("name").innerHTML = tab[0]['name'];
+//     document.getElementById("description").innerHTML = tab[0]['description'];
+//     let prix = tab[0]['price'];
+//     document.getElementById("price").innerHTML = prix /100;
+
+//     let im = document.getElementById("image");
+//     let imUrl = tab[0]['imageUrl'];
+//     im.src = imUrl;
+// };
 
 
+    function showTeddies(tab){
+        for (i=0; i < tab.length; i++) {
+        document.getElementById("name").innerHTML = tab[i]['name'];
+        document.getElementById("description").innerHTML = tab[i]['description'];
+        let prix = tab[i]['price'];
+        document.getElementById("price").innerHTML = prix /100;
+        let im = document.getElementById("image");
+        let imUrl = tab[i]['imageUrl'];
+        im.src = imUrl;
+        }
+        for(i=0; i < (tab.length -1); i++){
+            let item = document.getElementById("container");
+            let clone = item.cloneNode(true);
+            document.querySelector("body").appendChild(clone);
+        }
+    };
+
+    
