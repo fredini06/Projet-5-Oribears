ea438d530d085cdc774d8a16337871a7c4fb23c0
Frederic Iniesta <fredini06@gmail.com>
2020-08-27 22:41:18 +0200

    Augmenter baisser les quantités dans le panier

--- js/scriptPanier.js
+++ js/scriptPanier.js
@@ -2,19 +2,35 @@ let tabRec = localStorage.getItem("TabStore");
 console.log(tabRec);
 let tabRecJson = JSON.parse(tabRec);
 console.log(tabRecJson);
-console.log(tabRecJson.prix);
+console.log(tabRecJson.nom);
 
 let tr = document.createElement("tr");
-let htmlTr = `<tr><td>${tabRecJson.nom}</td><td>${tabRecJson.couleur}</td><td>${tabRecJson.prix}</td><input type="text" id="qte" value="1" /></tr>`;
+let htmlTr = `<tr><td>${tabRecJson.nom}</td><td>${tabRecJson.couleur}</td><td>${tabRecJson.prix} €</td><ion-icon name="caret-back-outline" id="decBtn"></ion-icon><input type="text" id="qte" value="1" /><ion-icon name="caret-forward-outline" id="incBtn"></ion-icon></tr><td id="price">${tabRecJson.prix} €</td>`;
 tr.innerHTML = htmlTr;
 document.getElementById('table__body').appendChild(tr);
 
 let qte = document.getElementById('qte');
-console.log(qte);
+// console.log("input= ", qte);
+// console.log(typeof tabRecJson.prix);
 
-qte.onchange = function() {
-    htmlTr += `<tr><td>${tabRecJson.prix}</td></tr>`;
-    tr.innerHTML = htmlTr;
-    document.getElementById('table__body').appendChild(tr);
-    console.log(qte.value);
-};
+let price = document.getElementById('price');
+let incBtn = document.getElementById('incBtn');
+incBtn.addEventListener('click', function() {
+    console.log("before", qte.value);
+    qte.value++;
+    console.log("after", qte.value);
+    console.log(price);
+    price.innerHTML = `${tabRecJson.prix * qte.value} €`;
+    console.log(price);
+});
+
+let decBtn = document.getElementById('decBtn');
+decBtn.addEventListener('click', function() {
+    console.log("before", qte.value);
+    qte.value--;
+    console.log("after", qte.value);
+    console.log(price);
+    price.innerHTML = `${tabRecJson.prix * qte.value} €`;
+    console.log(price);
+});
+
